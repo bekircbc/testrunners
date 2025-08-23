a3e123c7658fcab1711e9538bdbc782917e5e96f
Bekir Cebeci <62771823+bekircbc@users.noreply.github.com>
2025-08-24 00:59:53 +0200

    Update providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,6 @@
-import { NextIntlProvider } from 'next-intl';
+import { NextIntlClientProvider } from 'next-intl';
 import { ReactNode } from 'react';
 
 export default function Providers({ children, messages }: { children: ReactNode; messages: any }) {
-  return <NextIntlProvider messages={messages}>{children}</NextIntlProvider>;
+  return <NextIntlClientProvider messages={messages}>{children}</NextIntlClientProvider>;
 }
