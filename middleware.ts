67a781bd07ec998e5c8970ead4b9e7341b98d524
Bekir Cebeci <62771823+bekircbc@users.noreply.github.com>
2025-08-24 13:57:02 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+const locales = ['de', 'en', 'tr'];
+
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
-  // Sadece anasayfa isteğinde yönlendir
-  if (pathname === '/') {
-    return NextResponse.redirect(new URL('/de', request.url));
+  if (locales.some((locale) => pathname === `/${locale}` || pathname.startsWith(`/${locale}/`))) {
+    return NextResponse.redirect(new URL('/', request.url));
   }
 
   return NextResponse.next();
