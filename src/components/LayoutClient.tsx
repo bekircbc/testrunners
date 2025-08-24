bd4c079cb64e60f4f01dd83b6552be9df94d5559
Bekir Cebeci <62771823+bekircbc@users.noreply.github.com>
2025-08-24 05:13:06 +0200

    Update LayoutClient.tsx

--- src/components/LayoutClient.tsx
+++ src/components/LayoutClient.tsx
@@ -3,10 +3,10 @@ import Header from "./Header";
 import Footer from "./Footer";
 import ClientMuiProvider from "./ClientMuiProvider";
 
-export default function LayoutClient({ children }: { children: React.ReactNode }) {
+export default function LayoutClient({ children, locale }: { children: React.ReactNode, locale: string }) {
   return (
     <ClientMuiProvider>
-      <Header />
+      <Header locale={locale}/>
       <main style={{ flex: 1 }}>
         {children}
       </main>
