d73db0a8b7022dce73926a6a2a869f8d9cf28c12
Bekir Cebeci <62771823+bekircbc@users.noreply.github.com>
2026-07-02 12:45:58 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
 // next.config.js
 const withNextIntl = require('next-intl/plugin')(
-  './next-intl.config.js' // config dosyasının yolu
+  './next-intl.config.js' // config file path
 );
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  output: 'standalone'
 };
 
 module.exports = withNextIntl(nextConfig);
